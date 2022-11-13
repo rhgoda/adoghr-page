03ce062d7c62184af94125296e82cf72883a81e0
adoghr <adoghr.w@gmail.com>
2022-11-13 22:15:08 +0300

    dogspinning fix path

--- dogspinning/dogspinning.js
+++ dogspinning/dogspinning.js
@@ -1,5 +1,8 @@
 import express from "express";
 import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 let router = express.Router()
 
